850db01c9a43886a0e0c551262b0eb3ffbac269a
tawsiftorabi <tawsiftorabi.jr@gmail.com>
2025-03-11 21:40:14 +0600

    updated achivements page

--- frontend/scripts/scipt.js
+++ frontend/scripts/scipt.js
@@ -2,14 +2,14 @@
 let renderer = new THREE.WebGLRenderer();
 let scene = new THREE.Scene();
 let aspect = window.innerWidth / window.innerHeight;
-let camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 1500);
+let camera = new THREE.PerspectiveCamera(55, aspect, 0.1, 1500);
 let cameraRotation = 0;
 let cameraRotationSpeed = 0.001;
 let cameraAutoRotation = true;
 let orbitControls = new THREE.OrbitControls(camera);
 
 // Lights
-let spotLight = new THREE.SpotLight(0xffffff, 1, 0, 10, 2);
+let spotLight = new THREE.SpotLight(0xffffff, 1, 0, 15, 2);
 
 // Texture Loader
 let textureLoader = new THREE.TextureLoader();
